8591bca8b76a0e37edbc7e6f354c5e4abe403cff
barrosdvlpr <barrosdvlpr@gmail.com>
2023-02-16 09:08:42 -0300

    refactor-action-type-constants-and-action-creators

--- src/store/rates.js
+++ src/store/rates.js
@@ -6,9 +6,9 @@ const initialState = {
 export function ratesReducer(state = initialState, action) {
 
     switch(action.type) {
-        case "rates/amountChanged":
+        case AMOUNT_CHANGED:
             return { ...state, amount: action.payload }
-        case "rates/currencyCodeChanged":
+        case CURRENCY_CODE_CHANGED:
             return { ...state, currencyCode: action.payload }
         default: return state;
     }
@@ -18,3 +18,20 @@ export function ratesReducer(state = initialState, action) {
 
 export const getAmount = state => state.rates.amount
 export const getCurrecyCode = state => state.rates.currencyCode
+
+// action types
+
+export const AMOUNT_CHANGED = "rates/amountChanged"
+export const CURRENCY_CODE_CHANGED = "rates/currencyCodeChanged"
+
+// action creators
+
+export const changeAmount = (amount) => ({
+    type: AMOUNT_CHANGED,
+    payload: amount,
+})
+
+export const changeCurrecyCode = (currencyCode) => ({
+    type: CURRENCY_CODE_CHANGED,
+    payload: currencyCode,
+})
